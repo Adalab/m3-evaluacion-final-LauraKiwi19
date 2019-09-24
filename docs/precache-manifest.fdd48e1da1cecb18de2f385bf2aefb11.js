a92d41dfa3632d0909b0ec53b8b6e54f1ee6ce5a
Laura Ruiz <laura.ruizmazorra@gmail.com>
2019-09-24 18:26:06 +0200

    add docs

---
+++ docs/precache-manifest.fdd48e1da1cecb18de2f385bf2aefb11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65aeaf02b4adab97b98422b51d670f54",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a8ae4738d83b1cf1923d",
+    "url": "./static/css/main.39c1d29e.chunk.css"
+  },
+  {
+    "revision": "ff9732844be3b77cd298",
+    "url": "./static/js/2.21a1bf57.chunk.js"
+  },
+  {
+    "revision": "a8ae4738d83b1cf1923d",
+    "url": "./static/js/main.7712cdd9.chunk.js"
+  },
+  {
+    "revision": "8daef009ffb9af5bb90f",
+    "url": "./static/js/runtime-main.4c39bf8d.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/rickmortylogo.75509f2e.png"
+  }
+]);
